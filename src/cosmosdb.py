3bcd2b18ed7826d2d85ec122c2c711afdc5cb843
Isabel Cabezas <isabelcabezasm@outlook.com>
2025-02-19 06:48:34 +0000

    Refactor Cosmos DB integration to use ContainerProxy and improve cap insertion logic

--- src/cosmosdb.py
+++ src/cosmosdb.py
@@ -1,10 +1,12 @@
 
 import os
-from azure.cosmos import CosmosClient, exceptions
+from azure.cosmos import CosmosClient, exceptions, ContainerProxy
 
 from cap import Cap
 import common
 
+# create type called Container that wrapps a ContainerProxy
+Container = ContainerProxy
 
 def get_cosmos_client():
     try:
@@ -23,7 +25,7 @@ def get_database():
         print(f"Database {database_name} not found.")
         raise e
 
-def get_container():
+def get_container() -> Container:
     try:
         container_name = os.getenv('CONTAINER_NAME')
         container = get_database().get_container_client(container_name)
@@ -32,14 +34,20 @@ def get_container():
         print(f"Container {container_name} not found.")
         raise e
 
-def insert_cap(cap:Cap):
+def insert_cap(cap:Cap, container_client: Container):
     try:        
         cap_dictionary = cap.to_dict()
-        container_client = get_container()
-        _ = container_client.create_item(body=cap_dictionary)
-        
-    except exceptions.CosmosHttpResponseError as e:
-        print(f"Failed to insert cap: {e}")
+
+        # check if a cap with the same id already exists        
+        # if container_client.query_items(query=f"SELECT * FROM c WHERE c.id = '{cap_dictionary['id']}'"):
+        if container_client.read_item(item=cap_dictionary['id'], partition_key=cap_dictionary['country']):
+            print(f"Cap {cap_dictionary['id']} already exists.")
+    except exceptions.CosmosResourceNotFoundError:
+        try:
+            # if the item does not exist, create it
+            _ = container_client.create_item(body=cap_dictionary)                    
+        except exceptions.CosmosHttpResponseError as e:
+            print(f"Failed to insert cap: {e}")
 
 if __name__ == "__main__":
 
